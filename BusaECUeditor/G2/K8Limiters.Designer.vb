922c9d99a3bfcadad86d1f384ec7d18d1b2f0ab2
PetriK <PetriK@localhost>
2010-06-23 22:45:02 +0300

    Gen2 - Some commenting on limiters to clarify limiter types, no real changes

--- BusaECUeditor/G2/K8Limiters.Designer.vb
+++ BusaECUeditor/G2/K8Limiters.Designer.vb
@@ -34,7 +34,7 @@ Partial Class K8Limiters
         'C_gearlimiter
         '
         Me.C_gearlimiter.AutoSize = True
-        Me.C_gearlimiter.Location = New System.Drawing.Point(116, 57)
+        Me.C_gearlimiter.Location = New System.Drawing.Point(142, 57)
         Me.C_gearlimiter.Name = "C_gearlimiter"
         Me.C_gearlimiter.Size = New System.Drawing.Size(15, 14)
         Me.C_gearlimiter.TabIndex = 19
@@ -45,9 +45,9 @@ Partial Class K8Limiters
         Me.Label3.AutoSize = True
         Me.Label3.Location = New System.Drawing.Point(12, 58)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(59, 13)
+        Me.Label3.Size = New System.Drawing.Size(80, 13)
         Me.Label3.TabIndex = 18
-        Me.Label3.Text = "Gear limiter"
+        Me.Label3.Text = "Fuel gear limiter"
         '
         'Label2
         '
@@ -63,15 +63,15 @@ Partial Class K8Limiters
         Me.RPM.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
         Me.RPM.Enabled = False
         Me.RPM.FormattingEnabled = True
-        Me.RPM.Location = New System.Drawing.Point(116, 18)
+        Me.RPM.Location = New System.Drawing.Point(142, 18)
         Me.RPM.Name = "RPM"
-        Me.RPM.Size = New System.Drawing.Size(121, 21)
+        Me.RPM.Size = New System.Drawing.Size(84, 21)
         Me.RPM.TabIndex = 16
         '
         'Hardcut
         '
         Me.Hardcut.AutoSize = True
-        Me.Hardcut.Location = New System.Drawing.Point(116, 89)
+        Me.Hardcut.Location = New System.Drawing.Point(142, 89)
         Me.Hardcut.Name = "Hardcut"
         Me.Hardcut.Size = New System.Drawing.Size(15, 14)
         Me.Hardcut.TabIndex = 21
@@ -82,14 +82,14 @@ Partial Class K8Limiters
         Me.Label1.AutoSize = True
         Me.Label1.Location = New System.Drawing.Point(12, 90)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(94, 13)
+        Me.Label1.Size = New System.Drawing.Size(100, 13)
         Me.Label1.TabIndex = 22
-        Me.Label1.Text = "Softcut or Hardcut"
+        Me.Label1.Text = "Fuel soft or Hardcut"
         '
         'Label5
         '
         Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(11, 166)
+        Me.Label5.Location = New System.Drawing.Point(12, 119)
         Me.Label5.Name = "Label5"
         Me.Label5.Size = New System.Drawing.Size(364, 13)
         Me.Label5.TabIndex = 26
@@ -107,7 +107,7 @@ Partial Class K8Limiters
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(387, 188)
+        Me.ClientSize = New System.Drawing.Size(387, 144)
         Me.Controls.Add(Me.Label5)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.Hardcut)
