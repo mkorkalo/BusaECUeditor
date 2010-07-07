1ab8aff55345b55a68418cb18604ffce64404109
jkwool <jkwool@localhost>
2010-07-07 21:47:20 +1000

    Added Gen 1 files to Publish

---
+++ BusaECUeditor/Bking/BkingShifter.Designer.vb
@@ -0,0 +1,402 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class BKingShifter
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(BKingShifter))
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox
+        Me.Label9 = New System.Windows.Forms.Label
+        Me.Label8 = New System.Windows.Forms.Label
+        Me.Label7 = New System.Windows.Forms.Label
+        Me.Label6 = New System.Windows.Forms.Label
+        Me.Label5 = New System.Windows.Forms.Label
+        Me.Label4 = New System.Windows.Forms.Label
+        Me.C_igncut = New System.Windows.Forms.CheckBox
+        Me.Label3 = New System.Windows.Forms.Label
+        Me.C_Fuelcut = New System.Windows.Forms.CheckBox
+        Me.L_killcountdelay = New System.Windows.Forms.Label
+        Me.L_minkillactive = New System.Windows.Forms.Label
+        Me.T_killcountdelay = New System.Windows.Forms.TextBox
+        Me.T_minkillactive = New System.Windows.Forms.TextBox
+        Me.C_killtime = New System.Windows.Forms.ComboBox
+        Me.Label13 = New System.Windows.Forms.Label
+        Me.T_Gear6 = New System.Windows.Forms.TextBox
+        Me.T_Gear5 = New System.Windows.Forms.TextBox
+        Me.T_Gear4 = New System.Windows.Forms.TextBox
+        Me.T_Gear3 = New System.Windows.Forms.TextBox
+        Me.T_Gear2 = New System.Windows.Forms.TextBox
+        Me.T_Gear1 = New System.Windows.Forms.TextBox
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.C_shifter_activation = New System.Windows.Forms.CheckBox
+        Me.T_3000 = New System.Windows.Forms.TextBox
+        Me.HelpProvider1 = New System.Windows.Forms.HelpProvider
+        Me.L_shifterver = New System.Windows.Forms.Label
+        Me.T_4000 = New System.Windows.Forms.TextBox
+        Me.PrintForm1 = New Microsoft.VisualBasic.PowerPacks.Printing.PrintForm(Me.components)
+        Me.T_5000 = New System.Windows.Forms.TextBox
+        Me.T_6000 = New System.Windows.Forms.TextBox
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Label9)
+        Me.GroupBox2.Controls.Add(Me.Label8)
+        Me.GroupBox2.Controls.Add(Me.Label7)
+        Me.GroupBox2.Controls.Add(Me.Label6)
+        Me.GroupBox2.Controls.Add(Me.Label5)
+        Me.GroupBox2.Controls.Add(Me.Label4)
+        Me.GroupBox2.Controls.Add(Me.C_igncut)
+        Me.GroupBox2.Controls.Add(Me.Label3)
+        Me.GroupBox2.Controls.Add(Me.C_Fuelcut)
+        Me.GroupBox2.Controls.Add(Me.L_killcountdelay)
+        Me.GroupBox2.Controls.Add(Me.L_minkillactive)
+        Me.GroupBox2.Controls.Add(Me.T_killcountdelay)
+        Me.GroupBox2.Controls.Add(Me.T_minkillactive)
+        Me.GroupBox2.Controls.Add(Me.C_killtime)
+        Me.GroupBox2.Controls.Add(Me.Label13)
+        Me.GroupBox2.Controls.Add(Me.T_Gear6)
+        Me.GroupBox2.Controls.Add(Me.T_Gear5)
+        Me.GroupBox2.Controls.Add(Me.T_Gear4)
+        Me.GroupBox2.Controls.Add(Me.T_Gear3)
+        Me.GroupBox2.Controls.Add(Me.T_Gear2)
+        Me.GroupBox2.Controls.Add(Me.T_Gear1)
+        Me.GroupBox2.Location = New System.Drawing.Point(3, 31)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(524, 120)
+        Me.GroupBox2.TabIndex = 58
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Shifter settings"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(470, 19)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(36, 13)
+        Me.Label9.TabIndex = 49
+        Me.Label9.Text = "Gear6"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(428, 19)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(36, 13)
+        Me.Label8.TabIndex = 48
+        Me.Label8.Text = "Gear5"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(387, 19)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(36, 13)
+        Me.Label7.TabIndex = 47
+        Me.Label7.Text = "Gear4"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(346, 19)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(36, 13)
+        Me.Label6.TabIndex = 46
+        Me.Label6.Text = "Gear3"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(305, 19)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(36, 13)
+        Me.Label5.TabIndex = 45
+        Me.Label5.Text = "Gear2"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(264, 19)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(36, 13)
+        Me.Label4.TabIndex = 44
+        Me.Label4.Text = "Gear1"
+        '
+        'C_igncut
+        '
+        Me.C_igncut.AutoSize = True
+        Me.C_igncut.Location = New System.Drawing.Point(123, 98)
+        Me.C_igncut.Name = "C_igncut"
+        Me.C_igncut.Size = New System.Drawing.Size(88, 17)
+        Me.C_igncut.TabIndex = 43
+        Me.C_igncut.Text = "Igncut active"
+        Me.C_igncut.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(6, 76)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(43, 13)
+        Me.Label3.TabIndex = 42
+        Me.Label3.Text = "Kill type"
+        '
+        'C_Fuelcut
+        '
+        Me.C_Fuelcut.AutoSize = True
+        Me.C_Fuelcut.Location = New System.Drawing.Point(123, 75)
+        Me.C_Fuelcut.Name = "C_Fuelcut"
+        Me.C_Fuelcut.Size = New System.Drawing.Size(93, 17)
+        Me.C_Fuelcut.TabIndex = 41
+        Me.C_Fuelcut.Text = "Fuelcut active"
+        Me.C_Fuelcut.UseVisualStyleBackColor = True
+        '
+        'L_killcountdelay
+        '
+        Me.L_killcountdelay.AutoSize = True
+        Me.L_killcountdelay.Location = New System.Drawing.Point(413, 79)
+        Me.L_killcountdelay.Name = "L_killcountdelay"
+        Me.L_killcountdelay.Size = New System.Drawing.Size(51, 13)
+        Me.L_killcountdelay.TabIndex = 40
+        Me.L_killcountdelay.Text = "Next shift"
+        '
+        'L_minkillactive
+        '
+        Me.L_minkillactive.AutoSize = True
+        Me.L_minkillactive.Location = New System.Drawing.Point(314, 79)
+        Me.L_minkillactive.Name = "L_minkillactive"
+        Me.L_minkillactive.Size = New System.Drawing.Size(54, 13)
+        Me.L_minkillactive.TabIndex = 39
+        Me.L_minkillactive.Text = "Activation"
+        '
+        'T_killcountdelay
+        '
+        Me.T_killcountdelay.Location = New System.Drawing.Point(470, 76)
+        Me.T_killcountdelay.Name = "T_killcountdelay"
+        Me.T_killcountdelay.Size = New System.Drawing.Size(35, 20)
+        Me.T_killcountdelay.TabIndex = 38
+        '
+        'T_minkillactive
+        '
+        Me.T_minkillactive.Location = New System.Drawing.Point(373, 76)
+        Me.T_minkillactive.Name = "T_minkillactive"
+        Me.T_minkillactive.Size = New System.Drawing.Size(35, 20)
+        Me.T_minkillactive.TabIndex = 37
+        '
+        'C_killtime
+        '
+        Me.C_killtime.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.C_killtime.FormattingEnabled = True
+        Me.C_killtime.Location = New System.Drawing.Point(121, 32)
+        Me.C_killtime.MaxDropDownItems = 12
+        Me.C_killtime.Name = "C_killtime"
+        Me.C_killtime.Size = New System.Drawing.Size(85, 21)
+        Me.C_killtime.TabIndex = 26
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Location = New System.Drawing.Point(6, 35)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(71, 13)
+        Me.Label13.TabIndex = 25
+        Me.Label13.Text = "Kill time (~ms)"
+        '
+        'T_Gear6
+        '
+        Me.T_Gear6.Location = New System.Drawing.Point(470, 35)
+        Me.T_Gear6.Name = "T_Gear6"
+        Me.T_Gear6.Size = New System.Drawing.Size(35, 20)
+        Me.T_Gear6.TabIndex = 9
+        '
+        'T_Gear5
+        '
+        Me.T_Gear5.Location = New System.Drawing.Point(429, 35)
+        Me.T_Gear5.Name = "T_Gear5"
+        Me.T_Gear5.Size = New System.Drawing.Size(35, 20)
+        Me.T_Gear5.TabIndex = 8
+        '
+        'T_Gear4
+        '
+        Me.T_Gear4.Location = New System.Drawing.Point(388, 35)
+        Me.T_Gear4.Name = "T_Gear4"
+        Me.T_Gear4.Size = New System.Drawing.Size(35, 20)
+        Me.T_Gear4.TabIndex = 7
+        '
+        'T_Gear3
+        '
+        Me.T_Gear3.Location = New System.Drawing.Point(347, 35)
+        Me.T_Gear3.Name = "T_Gear3"
+        Me.T_Gear3.Size = New System.Drawing.Size(35, 20)
+        Me.T_Gear3.TabIndex = 6
+        '
+        'T_Gear2
+        '
+        Me.T_Gear2.Location = New System.Drawing.Point(306, 35)
+        Me.T_Gear2.Name = "T_Gear2"
+        Me.T_Gear2.Size = New System.Drawing.Size(35, 20)
+        Me.T_Gear2.TabIndex = 5
+        '
+        'T_Gear1
+        '
+        Me.T_Gear1.Location = New System.Drawing.Point(265, 35)
+        Me.T_Gear1.Name = "T_Gear1"
+        Me.T_Gear1.Size = New System.Drawing.Size(35, 20)
+        Me.T_Gear1.TabIndex = 4
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(339, 8)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(188, 13)
+        Me.Label2.TabIndex = 53
+        Me.Label2.Text = "Shifter module  v         ready for testing"
+        '
+        'C_shifter_activation
+        '
+        Me.C_shifter_activation.AutoSize = True
+        Me.C_shifter_activation.Location = New System.Drawing.Point(122, 8)
+        Me.C_shifter_activation.Name = "C_shifter_activation"
+        Me.C_shifter_activation.Size = New System.Drawing.Size(106, 17)
+        Me.C_shifter_activation.TabIndex = 51
+        Me.C_shifter_activation.Text = "Shifter not active"
+        Me.C_shifter_activation.UseVisualStyleBackColor = True
+        '
+        'T_3000
+        '
+        Me.T_3000.Location = New System.Drawing.Point(422, 270)
+        Me.T_3000.Name = "T_3000"
+        Me.T_3000.Size = New System.Drawing.Size(35, 20)
+        Me.T_3000.TabIndex = 57
+        '
+        'L_shifterver
+        '
+        Me.L_shifterver.AutoSize = True
+        Me.L_shifterver.Location = New System.Drawing.Point(420, 8)
+        Me.L_shifterver.Name = "L_shifterver"
+        Me.L_shifterver.Size = New System.Drawing.Size(25, 13)
+        Me.L_shifterver.TabIndex = 59
+        Me.L_shifterver.Text = "000"
+        '
+        'T_4000
+        '
+        Me.T_4000.Location = New System.Drawing.Point(381, 270)
+        Me.T_4000.Name = "T_4000"
+        Me.T_4000.Size = New System.Drawing.Size(35, 20)
+        Me.T_4000.TabIndex = 56
+        '
+        'PrintForm1
+        '
+        Me.PrintForm1.DocumentName = "document"
+        Me.PrintForm1.Form = Me
+        Me.PrintForm1.PrintAction = System.Drawing.Printing.PrintAction.PrintToPrinter
+        Me.PrintForm1.PrinterSettings = CType(resources.GetObject("PrintForm1.PrinterSettings"), System.Drawing.Printing.PrinterSettings)
+        Me.PrintForm1.PrintFileName = Nothing
+        '
+        'T_5000
+        '
+        Me.T_5000.Location = New System.Drawing.Point(340, 270)
+        Me.T_5000.Name = "T_5000"
+        Me.T_5000.Size = New System.Drawing.Size(35, 20)
+        Me.T_5000.TabIndex = 55
+        '
+        'T_6000
+        '
+        Me.T_6000.Location = New System.Drawing.Point(299, 270)
+        Me.T_6000.Name = "T_6000"
+        Me.T_6000.Size = New System.Drawing.Size(35, 20)
+        Me.T_6000.TabIndex = 54
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(7, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(86, 13)
+        Me.Label1.TabIndex = 52
+        Me.Label1.Text = "Shifter activation"
+        '
+        'BKingShifter
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(535, 159)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.C_shifter_activation)
+        Me.Controls.Add(Me.T_3000)
+        Me.Controls.Add(Me.L_shifterver)
+        Me.Controls.Add(Me.T_4000)
+        Me.Controls.Add(Me.T_5000)
+        Me.Controls.Add(Me.T_6000)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Label2)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.KeyPreview = True
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "BKingShifter"
+        Me.ShowInTaskbar = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
+        Me.Text = "B-King Shifter (Thanks to Justin)"
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents Label9 As System.Windows.Forms.Label
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents C_igncut As System.Windows.Forms.CheckBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents C_Fuelcut As System.Windows.Forms.CheckBox
+    Friend WithEvents L_killcountdelay As System.Windows.Forms.Label
+    Friend WithEvents L_minkillactive As System.Windows.Forms.Label
+    Friend WithEvents T_killcountdelay As System.Windows.Forms.TextBox
+    Friend WithEvents T_minkillactive As System.Windows.Forms.TextBox
+    Friend WithEvents C_killtime As System.Windows.Forms.ComboBox
+    Friend WithEvents Label13 As System.Windows.Forms.Label
+    Friend WithEvents T_Gear6 As System.Windows.Forms.TextBox
+    Friend WithEvents T_Gear5 As System.Windows.Forms.TextBox
+    Friend WithEvents T_Gear4 As System.Windows.Forms.TextBox
+    Friend WithEvents T_Gear3 As System.Windows.Forms.TextBox
+    Friend WithEvents T_Gear2 As System.Windows.Forms.TextBox
+    Friend WithEvents T_Gear1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents C_shifter_activation As System.Windows.Forms.CheckBox
+    Friend WithEvents T_3000 As System.Windows.Forms.TextBox
+    Friend WithEvents HelpProvider1 As System.Windows.Forms.HelpProvider
+    Friend WithEvents L_shifterver As System.Windows.Forms.Label
+    Friend WithEvents T_4000 As System.Windows.Forms.TextBox
+    Friend WithEvents PrintForm1 As Microsoft.VisualBasic.PowerPacks.Printing.PrintForm
+    Friend WithEvents T_5000 As System.Windows.Forms.TextBox
+    Friend WithEvents T_6000 As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+End Class
