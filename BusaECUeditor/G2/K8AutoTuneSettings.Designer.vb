cc6008795256c88a7eba584936f0c83452410d9e
Justin <Justin@ASUS>
2011-07-10 22:38:49 +1000

    label change

--- BusaECUeditor/G2/K8AutoTuneSettings.Designer.vb
+++ BusaECUeditor/G2/K8AutoTuneSettings.Designer.vb
@@ -34,6 +34,18 @@ Partial Class K8AutoTuneSettings
         Me.Label5 = New System.Windows.Forms.Label()
         Me.Panel1 = New System.Windows.Forms.Panel()
         Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.NUD_MaxTimeOffset = New System.Windows.Forms.NumericUpDown()
+        Me.L_MaxTimeOffset = New System.Windows.Forms.Label()
+        Me.NUD_HeaderPipeLength = New System.Windows.Forms.NumericUpDown()
+        Me.L_HeaderPipeLength = New System.Windows.Forms.Label()
+        Me.NUD_HeaderPipeDiameter = New System.Windows.Forms.NumericUpDown()
+        Me.L_HeaderPipeDiameter = New System.Windows.Forms.Label()
+        Me.NUD_MaxEngineRPM = New System.Windows.Forms.NumericUpDown()
+        Me.L_MaxEngineRPM = New System.Windows.Forms.Label()
+        Me.NUD_EngineCapacity = New System.Windows.Forms.NumericUpDown()
+        Me.L_EngineCapacity = New System.Windows.Forms.Label()
+        Me.R_ExhaustGasVelocityOffset2 = New System.Windows.Forms.RadioButton()
+        Me.R_ExhaustGasVelocityOffset1 = New System.Windows.Forms.RadioButton()
         Me.Label12 = New System.Windows.Forms.Label()
         Me.NUD_AutoTuneMapSmoothingStrength = New System.Windows.Forms.NumericUpDown()
         Me.Label11 = New System.Windows.Forms.Label()
@@ -48,34 +60,22 @@ Partial Class K8AutoTuneSettings
         Me.TB_AutoTuneStrength = New System.Windows.Forms.TrackBar()
         Me.B_Cancel = New System.Windows.Forms.Button()
         Me.B_Ok = New System.Windows.Forms.Button()
-        Me.R_ExhaustGasVelocityOffset1 = New System.Windows.Forms.RadioButton()
-        Me.R_ExhaustGasVelocityOffset2 = New System.Windows.Forms.RadioButton()
-        Me.L_EngineCapacity = New System.Windows.Forms.Label()
-        Me.NUD_EngineCapacity = New System.Windows.Forms.NumericUpDown()
-        Me.L_MaxEngineRPM = New System.Windows.Forms.Label()
-        Me.NUD_MaxEngineRPM = New System.Windows.Forms.NumericUpDown()
-        Me.L_HeaderPipeDiameter = New System.Windows.Forms.Label()
-        Me.NUD_HeaderPipeDiameter = New System.Windows.Forms.NumericUpDown()
-        Me.L_HeaderPipeLength = New System.Windows.Forms.Label()
-        Me.NUD_HeaderPipeLength = New System.Windows.Forms.NumericUpDown()
-        Me.L_MaxTimeOffset = New System.Windows.Forms.Label()
-        Me.NUD_MaxTimeOffset = New System.Windows.Forms.NumericUpDown()
         CType(Me.NUD_AutoTuneMinAvgAFR, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.NUD_AutoTuneMaxAvgAFR, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.NUD_AutoTuneMaxPercentageFuelMapChange, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.NUD_AutoTuneMinNumberLoggedValuesInCell, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.Panel1.SuspendLayout()
         Me.Panel2.SuspendLayout()
+        CType(Me.NUD_MaxTimeOffset, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NUD_HeaderPipeLength, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NUD_HeaderPipeDiameter, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NUD_MaxEngineRPM, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NUD_EngineCapacity, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.NUD_AutoTuneMapSmoothingStrength, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.NUD_AutoTuneTimeWindow, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.NUD_AFRStdDev, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.NUD_ExhaustGasVelocityOffset, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.TB_AutoTuneStrength, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.NUD_EngineCapacity, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.NUD_MaxEngineRPM, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.NUD_HeaderPipeDiameter, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.NUD_HeaderPipeLength, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.NUD_MaxTimeOffset, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'NUD_AutoTuneMinAvgAFR
@@ -206,14 +206,133 @@ Partial Class K8AutoTuneSettings
         Me.Panel2.Size = New System.Drawing.Size(631, 255)
         Me.Panel2.TabIndex = 10
         '
+        'NUD_MaxTimeOffset
+        '
+        Me.NUD_MaxTimeOffset.Location = New System.Drawing.Point(555, 58)
+        Me.NUD_MaxTimeOffset.Maximum = New Decimal(New Integer() {500, 0, 0, 0})
+        Me.NUD_MaxTimeOffset.Minimum = New Decimal(New Integer() {50, 0, 0, 0})
+        Me.NUD_MaxTimeOffset.Name = "NUD_MaxTimeOffset"
+        Me.NUD_MaxTimeOffset.Size = New System.Drawing.Size(60, 20)
+        Me.NUD_MaxTimeOffset.TabIndex = 34
+        Me.NUD_MaxTimeOffset.Value = New Decimal(New Integer() {250, 0, 0, 0})
+        '
+        'L_MaxTimeOffset
+        '
+        Me.L_MaxTimeOffset.AutoSize = True
+        Me.L_MaxTimeOffset.Location = New System.Drawing.Point(372, 60)
+        Me.L_MaxTimeOffset.Name = "L_MaxTimeOffset"
+        Me.L_MaxTimeOffset.Size = New System.Drawing.Size(106, 13)
+        Me.L_MaxTimeOffset.TabIndex = 33
+        Me.L_MaxTimeOffset.Text = "Max Time Offset (ms)"
+        '
+        'NUD_HeaderPipeLength
+        '
+        Me.NUD_HeaderPipeLength.Location = New System.Drawing.Point(555, 158)
+        Me.NUD_HeaderPipeLength.Maximum = New Decimal(New Integer() {2000, 0, 0, 0})
+        Me.NUD_HeaderPipeLength.Minimum = New Decimal(New Integer() {500, 0, 0, 0})
+        Me.NUD_HeaderPipeLength.Name = "NUD_HeaderPipeLength"
+        Me.NUD_HeaderPipeLength.Size = New System.Drawing.Size(60, 20)
+        Me.NUD_HeaderPipeLength.TabIndex = 32
+        Me.NUD_HeaderPipeLength.Value = New Decimal(New Integer() {1000, 0, 0, 0})
+        '
+        'L_HeaderPipeLength
+        '
+        Me.L_HeaderPipeLength.AutoSize = True
+        Me.L_HeaderPipeLength.Location = New System.Drawing.Point(372, 158)
+        Me.L_HeaderPipeLength.Name = "L_HeaderPipeLength"
+        Me.L_HeaderPipeLength.Size = New System.Drawing.Size(127, 13)
+        Me.L_HeaderPipeLength.TabIndex = 31
+        Me.L_HeaderPipeLength.Text = "Header Pipe Length (mm)"
+        '
+        'NUD_HeaderPipeDiameter
+        '
+        Me.NUD_HeaderPipeDiameter.DecimalPlaces = 1
+        Me.NUD_HeaderPipeDiameter.Location = New System.Drawing.Point(555, 132)
+        Me.NUD_HeaderPipeDiameter.Maximum = New Decimal(New Integer() {60, 0, 0, 0})
+        Me.NUD_HeaderPipeDiameter.Minimum = New Decimal(New Integer() {30, 0, 0, 0})
+        Me.NUD_HeaderPipeDiameter.Name = "NUD_HeaderPipeDiameter"
+        Me.NUD_HeaderPipeDiameter.Size = New System.Drawing.Size(60, 20)
+        Me.NUD_HeaderPipeDiameter.TabIndex = 30
+        Me.NUD_HeaderPipeDiameter.Value = New Decimal(New Integer() {378, 0, 0, 65536})
+        '
+        'L_HeaderPipeDiameter
+        '
+        Me.L_HeaderPipeDiameter.AutoSize = True
+        Me.L_HeaderPipeDiameter.Location = New System.Drawing.Point(372, 132)
+        Me.L_HeaderPipeDiameter.Name = "L_HeaderPipeDiameter"
+        Me.L_HeaderPipeDiameter.Size = New System.Drawing.Size(136, 13)
+        Me.L_HeaderPipeDiameter.TabIndex = 29
+        Me.L_HeaderPipeDiameter.Text = "Header Pipe Diameter (mm)"
+        '
+        'NUD_MaxEngineRPM
+        '
+        Me.NUD_MaxEngineRPM.Increment = New Decimal(New Integer() {500, 0, 0, 0})
+        Me.NUD_MaxEngineRPM.Location = New System.Drawing.Point(555, 108)
+        Me.NUD_MaxEngineRPM.Maximum = New Decimal(New Integer() {18000, 0, 0, 0})
+        Me.NUD_MaxEngineRPM.Minimum = New Decimal(New Integer() {8000, 0, 0, 0})
+        Me.NUD_MaxEngineRPM.Name = "NUD_MaxEngineRPM"
+        Me.NUD_MaxEngineRPM.Size = New System.Drawing.Size(60, 20)
+        Me.NUD_MaxEngineRPM.TabIndex = 28
+        Me.NUD_MaxEngineRPM.Value = New Decimal(New Integer() {11000, 0, 0, 0})
+        '
+        'L_MaxEngineRPM
+        '
+        Me.L_MaxEngineRPM.AutoSize = True
+        Me.L_MaxEngineRPM.Location = New System.Drawing.Point(372, 108)
+        Me.L_MaxEngineRPM.Name = "L_MaxEngineRPM"
+        Me.L_MaxEngineRPM.Size = New System.Drawing.Size(90, 13)
+        Me.L_MaxEngineRPM.TabIndex = 27
+        Me.L_MaxEngineRPM.Text = "Max Engine RPM"
+        '
+        'NUD_EngineCapacity
+        '
+        Me.NUD_EngineCapacity.Location = New System.Drawing.Point(555, 84)
+        Me.NUD_EngineCapacity.Maximum = New Decimal(New Integer() {2000, 0, 0, 0})
+        Me.NUD_EngineCapacity.Minimum = New Decimal(New Integer() {500, 0, 0, 0})
+        Me.NUD_EngineCapacity.Name = "NUD_EngineCapacity"
+        Me.NUD_EngineCapacity.Size = New System.Drawing.Size(60, 20)
+        Me.NUD_EngineCapacity.TabIndex = 26
+        Me.NUD_EngineCapacity.Value = New Decimal(New Integer() {1340, 0, 0, 0})
+        '
+        'L_EngineCapacity
+        '
+        Me.L_EngineCapacity.AutoSize = True
+        Me.L_EngineCapacity.Location = New System.Drawing.Point(372, 84)
+        Me.L_EngineCapacity.Name = "L_EngineCapacity"
+        Me.L_EngineCapacity.Size = New System.Drawing.Size(105, 13)
+        Me.L_EngineCapacity.TabIndex = 25
+        Me.L_EngineCapacity.Text = "Engine Capacity (cc)"
+        '
+        'R_ExhaustGasVelocityOffset2
+        '
+        Me.R_ExhaustGasVelocityOffset2.AutoSize = True
+        Me.R_ExhaustGasVelocityOffset2.Location = New System.Drawing.Point(356, 37)
+        Me.R_ExhaustGasVelocityOffset2.Name = "R_ExhaustGasVelocityOffset2"
+        Me.R_ExhaustGasVelocityOffset2.Size = New System.Drawing.Size(188, 17)
+        Me.R_ExhaustGasVelocityOffset2.TabIndex = 24
+        Me.R_ExhaustGasVelocityOffset2.TabStop = True
+        Me.R_ExhaustGasVelocityOffset2.Text = "Exhaust Gas Velocity Offset (Time)"
+        Me.R_ExhaustGasVelocityOffset2.UseVisualStyleBackColor = True
+        '
+        'R_ExhaustGasVelocityOffset1
+        '
+        Me.R_ExhaustGasVelocityOffset1.AutoSize = True
+        Me.R_ExhaustGasVelocityOffset1.Location = New System.Drawing.Point(356, 12)
+        Me.R_ExhaustGasVelocityOffset1.Name = "R_ExhaustGasVelocityOffset1"
+        Me.R_ExhaustGasVelocityOffset1.Size = New System.Drawing.Size(189, 17)
+        Me.R_ExhaustGasVelocityOffset1.TabIndex = 23
+        Me.R_ExhaustGasVelocityOffset1.TabStop = True
+        Me.R_ExhaustGasVelocityOffset1.Text = "Exhaust Gas Velocity Offset (RPM)"
+        Me.R_ExhaustGasVelocityOffset1.UseVisualStyleBackColor = True
+        '
         'Label12
         '
         Me.Label12.AutoSize = True
         Me.Label12.Location = New System.Drawing.Point(18, 108)
         Me.Label12.Name = "Label12"
-        Me.Label12.Size = New System.Drawing.Size(160, 13)
+        Me.Label12.Size = New System.Drawing.Size(166, 13)
         Me.Label12.TabIndex = 22
-        Me.Label12.Text = "Map Smoothing Strength ( 0 - 9 )"
+        Me.Label12.Text = "Map Smoothing Strength ( 0 - 19 )"
         '
         'NUD_AutoTuneMapSmoothingStrength
         '
@@ -339,125 +458,6 @@ Partial Class K8AutoTuneSettings
         Me.B_Ok.Text = "OK"
         Me.B_Ok.UseVisualStyleBackColor = True
         '
-        'R_ExhaustGasVelocityOffset1
-        '
-        Me.R_ExhaustGasVelocityOffset1.AutoSize = True
-        Me.R_ExhaustGasVelocityOffset1.Location = New System.Drawing.Point(356, 12)
-        Me.R_ExhaustGasVelocityOffset1.Name = "R_ExhaustGasVelocityOffset1"
-        Me.R_ExhaustGasVelocityOffset1.Size = New System.Drawing.Size(189, 17)
-        Me.R_ExhaustGasVelocityOffset1.TabIndex = 23
-        Me.R_ExhaustGasVelocityOffset1.TabStop = True
-        Me.R_ExhaustGasVelocityOffset1.Text = "Exhaust Gas Velocity Offset (RPM)"
-        Me.R_ExhaustGasVelocityOffset1.UseVisualStyleBackColor = True
-        '
-        'R_ExhaustGasVelocityOffset2
-        '
-        Me.R_ExhaustGasVelocityOffset2.AutoSize = True
-        Me.R_ExhaustGasVelocityOffset2.Location = New System.Drawing.Point(356, 37)
-        Me.R_ExhaustGasVelocityOffset2.Name = "R_ExhaustGasVelocityOffset2"
-        Me.R_ExhaustGasVelocityOffset2.Size = New System.Drawing.Size(188, 17)
-        Me.R_ExhaustGasVelocityOffset2.TabIndex = 24
-        Me.R_ExhaustGasVelocityOffset2.TabStop = True
-        Me.R_ExhaustGasVelocityOffset2.Text = "Exhaust Gas Velocity Offset (Time)"
-        Me.R_ExhaustGasVelocityOffset2.UseVisualStyleBackColor = True
-        '
-        'L_EngineCapacity
-        '
-        Me.L_EngineCapacity.AutoSize = True
-        Me.L_EngineCapacity.Location = New System.Drawing.Point(372, 84)
-        Me.L_EngineCapacity.Name = "L_EngineCapacity"
-        Me.L_EngineCapacity.Size = New System.Drawing.Size(105, 13)
-        Me.L_EngineCapacity.TabIndex = 25
-        Me.L_EngineCapacity.Text = "Engine Capacity (cc)"
-        '
-        'NUD_EngineCapacity
-        '
-        Me.NUD_EngineCapacity.Location = New System.Drawing.Point(555, 84)
-        Me.NUD_EngineCapacity.Maximum = New Decimal(New Integer() {2000, 0, 0, 0})
-        Me.NUD_EngineCapacity.Minimum = New Decimal(New Integer() {500, 0, 0, 0})
-        Me.NUD_EngineCapacity.Name = "NUD_EngineCapacity"
-        Me.NUD_EngineCapacity.Size = New System.Drawing.Size(60, 20)
-        Me.NUD_EngineCapacity.TabIndex = 26
-        Me.NUD_EngineCapacity.Value = New Decimal(New Integer() {1340, 0, 0, 0})
-        '
-        'L_MaxEngineRPM
-        '
-        Me.L_MaxEngineRPM.AutoSize = True
-        Me.L_MaxEngineRPM.Location = New System.Drawing.Point(372, 108)
-        Me.L_MaxEngineRPM.Name = "L_MaxEngineRPM"
-        Me.L_MaxEngineRPM.Size = New System.Drawing.Size(90, 13)
-        Me.L_MaxEngineRPM.TabIndex = 27
-        Me.L_MaxEngineRPM.Text = "Max Engine RPM"
-        '
-        'NUD_MaxEngineRPM
-        '
-        Me.NUD_MaxEngineRPM.Increment = New Decimal(New Integer() {500, 0, 0, 0})
-        Me.NUD_MaxEngineRPM.Location = New System.Drawing.Point(555, 108)
-        Me.NUD_MaxEngineRPM.Maximum = New Decimal(New Integer() {18000, 0, 0, 0})
-        Me.NUD_MaxEngineRPM.Minimum = New Decimal(New Integer() {8000, 0, 0, 0})
-        Me.NUD_MaxEngineRPM.Name = "NUD_MaxEngineRPM"
-        Me.NUD_MaxEngineRPM.Size = New System.Drawing.Size(60, 20)
-        Me.NUD_MaxEngineRPM.TabIndex = 28
-        Me.NUD_MaxEngineRPM.Value = New Decimal(New Integer() {11000, 0, 0, 0})
-        '
-        'L_HeaderPipeDiameter
-        '
-        Me.L_HeaderPipeDiameter.AutoSize = True
-        Me.L_HeaderPipeDiameter.Location = New System.Drawing.Point(372, 132)
-        Me.L_HeaderPipeDiameter.Name = "L_HeaderPipeDiameter"
-        Me.L_HeaderPipeDiameter.Size = New System.Drawing.Size(136, 13)
-        Me.L_HeaderPipeDiameter.TabIndex = 29
-        Me.L_HeaderPipeDiameter.Text = "Header Pipe Diameter (mm)"
-        '
-        'NUD_HeaderPipeDiameter
-        '
-        Me.NUD_HeaderPipeDiameter.DecimalPlaces = 1
-        Me.NUD_HeaderPipeDiameter.Location = New System.Drawing.Point(555, 132)
-        Me.NUD_HeaderPipeDiameter.Maximum = New Decimal(New Integer() {60, 0, 0, 0})
-        Me.NUD_HeaderPipeDiameter.Minimum = New Decimal(New Integer() {30, 0, 0, 0})
-        Me.NUD_HeaderPipeDiameter.Name = "NUD_HeaderPipeDiameter"
-        Me.NUD_HeaderPipeDiameter.Size = New System.Drawing.Size(60, 20)
-        Me.NUD_HeaderPipeDiameter.TabIndex = 30
-        Me.NUD_HeaderPipeDiameter.Value = New Decimal(New Integer() {378, 0, 0, 65536})
-        '
-        'L_HeaderPipeLength
-        '
-        Me.L_HeaderPipeLength.AutoSize = True
-        Me.L_HeaderPipeLength.Location = New System.Drawing.Point(372, 158)
-        Me.L_HeaderPipeLength.Name = "L_HeaderPipeLength"
-        Me.L_HeaderPipeLength.Size = New System.Drawing.Size(127, 13)
-        Me.L_HeaderPipeLength.TabIndex = 31
-        Me.L_HeaderPipeLength.Text = "Header Pipe Length (mm)"
-        '
-        'NUD_HeaderPipeLength
-        '
-        Me.NUD_HeaderPipeLength.Location = New System.Drawing.Point(555, 158)
-        Me.NUD_HeaderPipeLength.Maximum = New Decimal(New Integer() {2000, 0, 0, 0})
-        Me.NUD_HeaderPipeLength.Minimum = New Decimal(New Integer() {500, 0, 0, 0})
-        Me.NUD_HeaderPipeLength.Name = "NUD_HeaderPipeLength"
-        Me.NUD_HeaderPipeLength.Size = New System.Drawing.Size(60, 20)
-        Me.NUD_HeaderPipeLength.TabIndex = 32
-        Me.NUD_HeaderPipeLength.Value = New Decimal(New Integer() {1000, 0, 0, 0})
-        '
-        'L_MaxTimeOffset
-        '
-        Me.L_MaxTimeOffset.AutoSize = True
-        Me.L_MaxTimeOffset.Location = New System.Drawing.Point(372, 60)
-        Me.L_MaxTimeOffset.Name = "L_MaxTimeOffset"
-        Me.L_MaxTimeOffset.Size = New System.Drawing.Size(106, 13)
-        Me.L_MaxTimeOffset.TabIndex = 33
-        Me.L_MaxTimeOffset.Text = "Max Time Offset (ms)"
-        '
-        'NUD_MaxTimeOffset
-        '
-        Me.NUD_MaxTimeOffset.Location = New System.Drawing.Point(555, 58)
-        Me.NUD_MaxTimeOffset.Maximum = New Decimal(New Integer() {500, 0, 0, 0})
-        Me.NUD_MaxTimeOffset.Minimum = New Decimal(New Integer() {50, 0, 0, 0})
-        Me.NUD_MaxTimeOffset.Name = "NUD_MaxTimeOffset"
-        Me.NUD_MaxTimeOffset.Size = New System.Drawing.Size(60, 20)
-        Me.NUD_MaxTimeOffset.TabIndex = 34
-        Me.NUD_MaxTimeOffset.Value = New Decimal(New Integer() {250, 0, 0, 0})
-        '
         'K8AutoTuneSettings
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
@@ -479,16 +479,16 @@ Partial Class K8AutoTuneSettings
         Me.Panel1.ResumeLayout(False)
         Me.Panel2.ResumeLayout(False)
         Me.Panel2.PerformLayout()
+        CType(Me.NUD_MaxTimeOffset, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NUD_HeaderPipeLength, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NUD_HeaderPipeDiameter, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NUD_MaxEngineRPM, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NUD_EngineCapacity, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.NUD_AutoTuneMapSmoothingStrength, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.NUD_AutoTuneTimeWindow, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.NUD_AFRStdDev, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.NUD_ExhaustGasVelocityOffset, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.TB_AutoTuneStrength, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.NUD_EngineCapacity, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.NUD_MaxEngineRPM, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.NUD_HeaderPipeDiameter, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.NUD_HeaderPipeLength, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.NUD_MaxTimeOffset, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
