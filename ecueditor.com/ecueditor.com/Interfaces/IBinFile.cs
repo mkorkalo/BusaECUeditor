1ad6438a8d04a2edad1d85ad6a2ccac5dde14b33
Justin <Justin@ASUS>
2012-01-02 22:30:35 +1000

    ecueditor.com v3.0 first commit

---
+++ ecueditor.com/ecueditor.com/Interfaces/IBinFile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ecueditor.com.Interfaces
+{
+    interface IBinFile
+    {
+        #region Properties
+
+        string ECUID
+        {
+            get;
+        }
+
+        string Model
+        {
+            get;
+        }
+
+        #endregion
+    }
+}
